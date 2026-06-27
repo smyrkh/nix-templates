12af49e40fb2869b29946a4f13b4c3f0b4f4661d
smyrkh <190591493+smyrkh@users.noreply.github.com>
2026-06-28 01:20:21 +0900

    feat(nodejs22): remove typescript-language-server

--- nodejs22/flake.nix
+++ nodejs22/flake.nix
@@ -5,26 +5,41 @@
     nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
   };
 
-  outputs = { self, nixpkgs }:
-    let
-      system = "aarch64-darwin";
-      pkgs = import nixpkgs { inherit system; };
-    in
-      {
-      devShells.${system}.default = pkgs.mkShell {
-        buildInputs = [
-          pkgs.nodejs_22
-          pkgs.typescript-language-server
-          pkgs.vscode-langservers-extracted
-        ];
-        shellHook = ''
+  outputs = {
+    self,
+    nixpkgs,
+  }: let
+    supportedSystems = [
+      "x86_64-linux"
+      "aarch64-linux"
+      "x86_64-darwin"
+      "aarch64-darwin"
+    ];
+    forAllSystems = nixpkgs.lib.genAttrs supportedSystems;
+  in {
+    devShells = forAllSystems (
+      system: let
+        pkgs = nixpkgs.legacyPackages.${system};
+      in {
+        default = pkgs.mkShell {
+          packages = [
+            pkgs.nodejs_22
+
+            pkgs.vscode-langservers-extracted
+          ];
+
+          shellHook = ''
+            export PATH="$PWD/node_modules/.bin:$PATH"
+
             if [ -f "package.json" ] && [ ! -d "node_modules" ]; then
               echo "📦 Installing dependencies from package.json..."
               npm install
             fi
 
             echo "🟢 Node.js $(node -v) Environment Ready!"
-        '';
-      };
-    };
+          '';
+        };
+      }
+    );
+  };
 }
