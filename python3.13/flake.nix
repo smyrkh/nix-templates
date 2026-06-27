fdfd224cbe272adb1bb570584459f6588f87806e
smyrkh <190591493+smyrkh@users.noreply.github.com>
2026-06-28 03:35:29 +0900

    refactor(python3.13): delete pkgs.python313.withPackages

--- python3.13/flake.nix
+++ python3.13/flake.nix
@@ -5,40 +5,67 @@
     nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
   };
 
-  outputs = { self, nixpkgs }:
-    let
-      system = "aarch64-darwin";
-      pkgs = import nixpkgs { inherit system; };
+  outputs = {
+    self,
+    nixpkgs,
+  }: let
+    supportedSystems = [
+      "x86_64-linux"
+      "aarch64-linux"
+      "x86_64-darwin"
+      "aarch64-darwin"
+    ];
+    forAllSystems = nixpkgs.lib.genAttrs supportedSystems;
+  in {
+    devShells = forAllSystems (
+      system: let
+        pkgs = nixpkgs.legacyPackages.${system};
+      in {
+        default = pkgs.mkShell {
+          packages = [
+            pkgs.python313
+            pkgs.uv
 
-      python = pkgs.python313.withPackages (p: with p; [
-        pwntools
-        pycryptodome
-      ]);
-    in
-      {
-      devShells.${system}.default = pkgs.mkShell {
-        buildInputs = [
-          python
-          pkgs.python313Packages.python-lsp-server
-          pkgs.black
-          pkgs.isort
+            pkgs.python313Packages.python-lsp-server
+            pkgs.black
+            pkgs.isort
 
-          pkgs.uv
-        ];
-        shellHook = ''
-          if [ ! -d ".venv" ]; then
-            echo "Creating virtual environment with uv..."
-            uv venv
-          fi
-          source .venv/bin/activate
+            pkgs.cmake
+            pkgs.pkg-config
+          ];
 
-          if [ -f "requirements.txt" ]; then
-            echo "Installing dependencies from requirements.txt..."
-            uv pip install -r requirements.txt
-          fi
+          shellHook = ''
+            setup_env() {
+              if [ -f "pyproject.toml" ]; then
+                echo "pyproject.toml found. Syncing environment with uv sync..."
+                uv sync
+                source .venv/bin/activate
+                echo "Python 3.13 Environment Ready !"
+                return
+              fi
 
-          echo "Python 3.13 Environment Ready !"
-        '';
-      };
-    };
+              if [ ! -d ".venv" ]; then
+                echo ".venv not found. Creating virtual environment with uv venv..."
+                uv venv
+              fi
+              source .venv/bin/activate
+
+              echo "Installing default packages..."
+              uv pip install \
+                pwntools \
+                pycryptodome
+
+              if [ -f "requirements.txt" ]; then
+                echo "requirements.txt found. Installing dependencies from requirements.txt..."
+                uv pip install -r requirements.txt
+              fi
+
+              echo "Python 3.13 Environment Ready !"
+            }
+            setup_env
+          '';
+        };
+      }
+    );
+  };
 }
