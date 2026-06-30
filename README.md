32fececf317d38e624249ccd79319851a2bcd0db
smyrkh <190591493+smyrkh@users.noreply.github.com>
2026-06-30 14:05:44 +0900

    feat(go): add universal environment

--- README.md
+++ README.md
@@ -6,13 +6,14 @@
 
 ```sh
 cd project
-nix develop ~/nix-templates/{envname} -c zsh
+nix develop ~/nix-templates/<envname> -c zsh
 ```
 
 ### permanent
 
 ```sh
 cd project
-echo "use flake ~/nix-templates/{envname}" > .envrc
+echo "source_env_if_exists .envrc.local" > .envrc
+echo "use flake ~/nix-templates/<envname>" > .envrc.local
 direnv allow
 ```
---
+++ go/flake.nix
@@ -0,0 +1,42 @@
+{
+  description = "Universal Go Environment";
+
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+  };
+
+  outputs = {
+    self,
+    nixpkgs,
+  }: let
+    supportedSystems = [
+      "x86_64-linux"
+      "aarch64-linux"
+      "x86_64-darwin"
+      "aarch64-darwin"
+    ];
+    forAllSystems = nixpkgs.lib.genAttrs supportedSystems;
+  in {
+    devShells = forAllSystems (
+      system: let
+        pkgs = nixpkgs.legacyPackages.${system};
+      in {
+        default = pkgs.mkShell {
+          packages = [
+            pkgs.go
+
+            pkgs.gopls
+            pkgs.gotools
+            pkgs.go-tools
+            pkgs.delve
+          ];
+
+          shellHook = ''
+            GO_VERSION=$(go version | awk '{sub(/go/, "", $3); print $3}')
+            echo "Go $GO_VERSION Environment Ready !"
+          '';
+        };
+      }
+    );
+  };
+}
